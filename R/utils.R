66b130578330f97fbf0ce2b394f3f5e704e74aa8
Palate Jean <jean.palate@nbb.be>
2023-12-12 10:40:00 +0100

    Export of likelihood functions

--- R/utils.R
+++ R/utils.R
@@ -53,6 +53,23 @@ test_anova<-function(ssm, dfm, ssr, dfr){
   return (statisticaltest(val, pval, desc))
 }
 
+#' Title
+#'
+#' @param nobs
+#' @param neffectiveobs
+#' @param nparams
+#' @param ll
+#' @param adjustedll
+#' @param aic
+#' @param aicc
+#' @param bic
+#' @param bicc
+#' @param ssq
+#'
+#' @return
+#' @export
+#'
+#' @examples
 likelihood<-function(nobs, neffectiveobs=NA, nparams=0, ll, adjustedll=NA, aic, aicc, bic, bicc, ssq){
 
   if (is.na(neffectiveobs)) neffectiveobs=nobs
