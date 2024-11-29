23fa95c93ef35952f7934a3259904479e6eadf36
palatej <palatejean@gmail.com>
2024-11-29 10:01:27 +0100

    New libraries Correction in periodc splines

--- R/splines.R
+++ R/splines.R
@@ -2,16 +2,19 @@
 #'
 #' @param order Order of the splines (4 for cubic)
 #' @param period Period of the splines (1 by default)
-#' @param knots Knots of the splines (in [0, period[]])
-#' @param pos Requested positions (in [0, period[]])
+#' @param knots Knots of the splines (in [0, period[)
+#' @param pos Requested positions (in [0, period[)
 #'
 #' @return A matrix (len(pos) x len(knots))
 #' @export
 #'
 #' @examples
+#' s<-periodic_splines(knots = c(0,.2,.3, .9,.95), pos=seq(0,1,0.01))
+#' matplot(s, type='l')
+
 periodic_splines <- function(order = 4, period = 1, knots, pos) {
     jm <- .jcall(
-        "jdplus/toolkit/base/r/math/BSplines", "Ljdplus/toolkit/base/core//math/matrices/Matrix;",
+        "jdplus/toolkit/base/r/math/BSplines", "Ljdplus/toolkit/base/api/math/matrices/Matrix;",
         "periodic", as.integer(order), as.numeric(period), .jarray(as.numeric(knots)), .jarray(as.numeric(pos))
     )
     res <- .jd2r_matrix(jm)
